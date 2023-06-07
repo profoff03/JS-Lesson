e91e4a76395f9a9dc2e44ab40bb73c158a8ded56
Sergey <lapin.yira@gmail.com>
2023-06-07 21:36:18 +0700

    first practice in JavaScript

---
+++ JS learning/scripts/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastMovieSaw1 = prompt("Один из последних просмотренный фильмов?", "");
+const lastMovieScore1 = prompt("На сколько оцените его?", "");
+
+personalMovieDB["movies"][lastMovieSaw1] = lastMovieScore1;
+
+const lastMovieSaw2 = prompt("Один из последних просмотренный фильмов?", "");
+const lastMovieScore2 = prompt("На сколько оцените его?", "");
+
+personalMovieDB["movies"][lastMovieSaw2] = lastMovieScore2;
+
+console.log(personalMovieDB["movies"]);
+
+
+
