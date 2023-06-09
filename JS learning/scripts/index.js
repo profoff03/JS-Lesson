cec077b8f38eff103aad7bb62e397cd3b3f0604e
Sergey <lapin.yira@gmail.com>
2023-06-09 20:21:21 +0700

    add example and ( && ) or( || ) operator

--- JS learning/scripts/index.js
+++ JS learning/scripts/index.js
@@ -1,26 +1,30 @@
-"use strict";
+// "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
 
-const lastMovieSaw1 = prompt("Один из последних просмотренный фильмов?", "");
-const lastMovieScore1 = prompt("На сколько оцените его?", "");
+// const lastMovieSaw1 = prompt("Один из последних просмотренный фильмов?", "");
+// const lastMovieScore1 = prompt("На сколько оцените его?", "");
 
-personalMovieDB["movies"][lastMovieSaw1] = lastMovieScore1;
+// personalMovieDB["movies"][lastMovieSaw1] = lastMovieScore1;
 
-const lastMovieSaw2 = prompt("Один из последних просмотренный фильмов?", "");
-const lastMovieScore2 = prompt("На сколько оцените его?", "");
+// const lastMovieSaw2 = prompt("Один из последних просмотренный фильмов?", "");
+// const lastMovieScore2 = prompt("На сколько оцените его?", "");
 
-personalMovieDB["movies"][lastMovieSaw2] = lastMovieScore2;
+// personalMovieDB["movies"][lastMovieSaw2] = lastMovieScore2;
 
-console.log(personalMovieDB["movies"]);
+// console.log(personalMovieDB);
+
+console.log(NaN || null || !3 || undefined || 5);
+console.log(NaN || null && !3 && undefined || 5);
+console.log(5 === 5 && 3 > 1 || 5);
 
 
 
