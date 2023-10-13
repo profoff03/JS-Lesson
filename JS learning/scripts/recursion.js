c4324648c7336d58ba22e9fc9d4806be72e4152e
profoff <lapin.yira@gmail.com>
2023-10-14 00:55:20 +0700

    added examples of cyclic approach and recursion

--- JS learning/scripts/recursion.js
+++ JS learning/scripts/recursion.js
@@ -24,7 +24,54 @@ let students = {
 }
 
 const getTotalProgressByIteration = function (data) {
-    console.log(Object.values(data));
+    let students = 0,
+        total = 0;
+
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
 }
 
-getTotalProgressByIteration(students);
+console.log(getTotalProgressByIteration(students));
+
+
+const getTotalProgressByRecursion = function (data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+        for (let subData of Object.values(data)) {
+            const subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];
+        }
+
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0] / result[1]);
